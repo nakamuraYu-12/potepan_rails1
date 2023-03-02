b0dccf83d1a962f7c94b8bfa26758039bde8db90
NakamuraY <nakamuray.pg@gmail.com>
2023-03-03 01:38:57 +0900

    新規予定追加/予定一覧表示機能実装

--- schedule/app/controllers/posts_controller.rb
+++ schedule/app/controllers/posts_controller.rb
@@ -1,4 +1,31 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :start_day, :end_day, :all_day, :memo))
+    if @post.save
+      redirect_to :posts
+    else
+      render "new"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
