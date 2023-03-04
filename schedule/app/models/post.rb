eb9e644eff2bdd40fcd99888343032deef9f6274
NakamuraY <nakamuray.pg@gmail.com>
2023-03-04 20:13:39 +0900

    バリデーション追加

--- schedule/app/models/post.rb
+++ schedule/app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length:{maximum: 20}
+  validates :start_day, presence: true
+  validates :end_day, presence: true
+  validates :memo,  length:{maximum: 500}
+  validate :start_end_check
+
+  def start_end_check
+    errors.add(:end_day, "は開始日より後に登録してください") unless
+    :start_date < :end_date
+  end
 end
